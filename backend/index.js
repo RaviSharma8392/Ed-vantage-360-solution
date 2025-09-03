59f665331e6d173041eea6ad6c845c6c8d628c78
Ravi <ravisharmabhimtal@gmail.com>
2025-09-03 15:33:52 +0530

    added frontend url

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+const FRONTEND_URL =" https://edvantage360solution.netlify.app"|| "http://localhost:5173";
 
 app.use(cors({
   origin: FRONTEND_URL,
